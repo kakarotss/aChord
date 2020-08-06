5e5a64d4f2d135195d1d61d4fe804f6afb8f269c
Arbab Ahmed <Arbab.Ahmed@academytrainee.com>
2020-08-06 16:07:45 +0100

    user controller mapped, adminUserService class added and implemented with methods for admin access

--- BackEnd/aChordBackEnd/src/main/java/com/qa/achord/controller/usersController.java
+++ BackEnd/aChordBackEnd/src/main/java/com/qa/achord/controller/usersController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.achord.entity.users;
 import com.qa.achord.service.usersService;
+import com.qa.achord.service.adminUserService;
 
 @CrossOrigin   //need this line just need it dont ask
 @RestController
@@ -20,6 +22,9 @@ public class usersController {
 	
 	@Autowired
 	usersService userService;
+	
+	@Autowired
+	adminUserService adminUserService;
 	
 	@PostMapping("/Register")
 	public String registration(@RequestBody users user) {
@@ -39,6 +44,32 @@ public class usersController {
 	@GetMapping("/myProfile")
 	public List<users> myProfile(@RequestBody users user) {
 		return userService.myProfile(user);
+	}
+	
+	@DeleteMapping("/deleteMyAccount")
+	public String deleteMyProfile(@RequestBody users user) {
+		return userService.deleteAccount(user.getUserId());
+	}
+	
+	
+	///////////////////////Admin tools Controller methods/////////////////
+	
+	@GetMapping("/allUsers")
+	public List<users> getAll() {
+		return adminUserService.adminGetUsers();
+	}
+	
+	@PostMapping("/addUsers")
+	public String adminAdd(@RequestBody users user) {
+		
+		adminUserService.adminAddUser(user);
+		return null;
+		
+	}
+	
+	@DeleteMapping("/deleteAccount")
+	public String deleteProfile(@RequestBody users user) {
+		return adminUserService.adminDeleteAccount(user.getUserId());
 	}
 
 }
---
+++ BackEnd/aChordBackEnd/src/main/java/com/qa/achord/service/adminUserService.java
@@ -0,0 +1,58 @@
+package com.qa.achord.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qa.achord.entity.users;
+import com.qa.achord.repository.usersRepo;
+
+@Service
+public class adminUserService {
+	
+	@Autowired
+	usersRepo userRepo;
+	
+	String message = "unknown";
+	
+	public List<users> adminGetUsers() {
+		List <users> allUsers = userRepo.findAll();
+	
+		return allUsers;
+	}
+	
+	
+	public String adminAddUser(users user) {
+		userRepo.save(user);
+		
+		if(userRepo.findByUsername(user.getUsername()).isEmpty()) {
+			message = "Insert failed, please try again";
+		}
+		else {
+			message = user.getUsername()+" Inserted";
+		}
+		//need check if it actually has been inserted probably using optional
+		return message;
+	}
+	
+	
+	
+
+
+public String adminDeleteAccount(int id) {
+	
+	 userRepo.deleteById(id);
+	 
+	 if(userRepo.existsById(id)) {
+		 message = "Account deletion failed";
+	 }
+	 else {
+		 message = "Account deleted successfully";
+	 }
+	return message;
+}
+
+	
+
+}
